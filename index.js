549d87bb2b9b20709ccd02a6cf4e5eebf5c25856
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-05-12 12:56:53 -0500

    Finished project

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
-require('dotenv').config();
+//const API_KEY = process.env.API_KEY; - Cant use for a live server yet - outside of the scope of the project
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+const API_KEY = ""; // Place your Giphy API key here
+
+async function getImage(query) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    const gifURL = data.data[0].images.original.url;
+
+    console.log(gifURL);
+
+    return(gifURL);
+}
+
+async function getRandomImage(query) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    
+    const randomIndex = Math.floor(Math.random() * data.data.length);
+
+    const gifURL = data.data[randomIndex].images.original.url;
+
+    console.log(gifURL);
+
+    return(gifURL);
+}
+
+async function getAllImages(query) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    
+    const allImages = data.data.map(gif => gif.images.original.url);
+
+    console.log(allImages);
+
+    return allImages;
+}
+
+async function helperGetImage(query) {
+    return await getImage(query);
+}
+
+async function helperGetRandomImage(query) {
+    return await getRandomImage(query);
+}
+
+async function helperGetAllImages(query) {
+    return await getAllImages(query);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#random-gif-button").addEventListener("click", async () => {
+        const smilingFriendsGif = await helperGetRandomImage("smiling-friends"); 
+        document.querySelector("#gif-container").innerHTML = `<img src="${smilingFriendsGif}" alt="Random GIF" />`;
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#search-button").addEventListener("click", async () => {
+        const userInput = document.querySelector("#search-input").value;
+        const allImages = await helperGetAllImages(userInput);
+        
+        document.querySelector(".usergifs").innerHTML = allImages.map(gif => `<img src="${gif}" alt="GIF" />`).join("");
+        
+    });
+});
